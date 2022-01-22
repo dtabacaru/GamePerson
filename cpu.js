fe3115e7ec6aaaa0a44e3e8f9be8e63fd7515e64
CANA <faackoof@yahoo.com>
2022-01-22 18:30:24 -0500

    This is the first two rows of the instruction set, just the framework, no actual instructions of passing of variables

--- cpu.js
+++ cpu.js
@@ -18,6 +18,103 @@ function IncrementCounter() {
 
 function ProcessInstruction(op) {
     switch (op) {
-
+    
+    case 0x00:    
+    break;
+    
+    case 0x01:    
+    break;
+    
+    case 0x02:    
+    break;
+    
+    case 0x03:    
+    break;
+    
+    case 0x04:    
+    break;
+    
+    case 0x05:    
+    break;
+    
+    case 0x06:   
+    break;
+    
+    case 0x07:    
+    break;
+    
+    case 0x08:    
+    break;
+    
+    case 0x09    
+    break;
+    
+    case 0x0A:    
+    break;
+    
+    case 0x0B:    
+    break;
+    
+    case 0x0C:    
+    break;
+    
+    case 0x0D:
+    break;
+    
+    case 0x0E:    
+    break;
+    
+    case 0x0F:    
+    break;
+    
+    case 0x10:    
+    break;
+    
+    case 0x11:
+    break;
+    
+    case 0x12:
+    break;
+    
+    case 0x13:
+    break;
+    
+    case 0x14:
+    break;
+    
+    case 0x15:
+    break;
+    
+    case 0x16:
+    break;
+    
+    case 0x17:
+    break;
+    
+    case 0x18:
+    break;
+    
+    case 0x19:
+    break;
+    
+    case 0x1A:
+    break;
+    
+    case 0x1B:
+    break;
+    
+    case 0x1C:
+    break;
+    
+    case 0x1D:
+    break;
+    
+    case 0x1E:
+    break;
+    
+    case 0x1F:
+    break;
+    
+    
     }
 }
