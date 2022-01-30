913b7b343a1ab64cf27fe635b236f6442f9e8767
CANA <faackoof@yahoo.com>
2022-01-30 16:40:41 -0500

    removed sound register

--- control.js
+++ control.js
@@ -14,17 +14,11 @@ Bit 0 - P10 Input: Right or A        (0=Pressed) (Read Only)
  *  there is a note about 0BJ registers on page 272
  *  9 = IME gameboy manual does not state an address, top of page 269
  *  need to decide whether I need if or else if statement for P14 & P15. probably else if since ROM polls each pin sequentially 
- *  not sure if I need to worry about waveformRAM register
- *  waveformRAM register uses addresses 0xFF30 -> 0xFF3F. each 8 bit address is split into 2 "steps"
- *  e.g. 0xFF30 bits 7-4 are "step 0" and bits 3-0 are "step 1"
- *      0xFF31 bits 7-4 are "step 2" and bits 3-0 are "step 3"
- *      0xFF3F bits 7-4 are "step 30" and bits 3-0 are "step 31"
  */
 
 
 var ControlRegister = []; // control register array
-var SoundRegister = []; // sound register array
-var WaveFormRAM = []; // waveform register
+
 var P14 = 1; // pulling P14 low polls directional buttons
 var P15 = 1; // pulling P15 low polls action buttons
 
@@ -68,31 +62,6 @@ const OBJ01 = 35; //OBJ0 address 0xFE00   LCD Y-coordinate
 const OBJ02 = 36; //OBJ0 address 0xFE01   LCD X-coordinate
 const OBJ03 = 37; //OBJ0 address 0xFE02   character code
 const OBJ04 = 38; //38 = OBJ0 address 0xFE03   attribute flag
-
-//#defines used for SoundRegister
-const NR10 = 0; //address 0xFF10
-const NR11 = 1; //address 0xFF11
-const NR12 = 2; //address 0xFF12
-const NR13 = 3; //address 0xFF13
-const NR14 = 4; //address 0xFF14
-const NR21 = 5; //address 0xFF16
-const NR22 = 5; //address 0xFF17
-const NR23 = 6; //address 0xFF18
-const NR24 = 7; //address 0xFF19
-const NR30 = 8; //address 0xFF1A
-const NR31 = 9; //address 0xFF1B    
-const NR32 = 10; //address 0xFF1C
-const NR33 = 11; //address 0xFF1D
-const NR34 = 12; //address 0xFF1E
-const NR41 = 13; //address 0xFF20
-const NR42 = 14; //address 0xFF21
-const NR43 = 15; //address 0xFF22
-const NR44 = 16; //address 0xFF23
-const NR50 = 17; //address 0xFF24
-const NR51 = 18; //address 0xFF25
-const NR52 = 19; //address 0xFF26
-
-
 
 
 
